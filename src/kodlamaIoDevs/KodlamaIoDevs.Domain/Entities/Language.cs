64be89f8da5c0ace4ae98748946bede638d12e3e
sergennkeles <65495192+sergennkeles@users.noreply.github.com>
2022-09-08 19:40:17 +0300

    Language entity created

---
+++ src/kodlamaIoDevs/KodlamaIoDevs.Domain/Entities/Language.cs
@@ -0,0 +1,28 @@
+﻿using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KodlamaIoDevs.Domain.Entities
+{
+    public class Language : Entity
+    {
+
+        public string Name { get; set; }
+        public Language()
+        {
+
+        }
+        public Language(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+
+
+
+    }
+}
