cee0e0bc6fa43cf3db1fd899ff18f9ad0bd56ab9
sergennkeles <65495192+sergennkeles@users.noreply.github.com>
2022-10-07 15:09:43 +0300

    refres token işlemi düzenlendi.

--- src/kodlamaIoDevs/WebAPI/Controllers/AuthController.cs
+++ src/kodlamaIoDevs/WebAPI/Controllers/AuthController.cs
@@ -1,4 +1,6 @@
-﻿using KodlamaIoDevs.Application.Features.Languages.Commands.CreateLanguage;
+﻿using Core.Security.Dtos;
+using Core.Security.Entities;
+using KodlamaIoDevs.Application.Features.Languages.Commands.CreateLanguage;
 using KodlamaIoDevs.Application.Features.Languages.Dtos;
 using KodlamaIoDevs.Application.Features.Users.Commands.LoginUser;
 using KodlamaIoDevs.Application.Features.Users.Commands.RegisterUser;
@@ -7,15 +9,28 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers
 {
-    public class AuthController:BaseController
+    public class AuthController : BaseController
     {
         [HttpPost("register")]
-        public async Task<IActionResult> Register([FromBody] RegisterUserCommand registerUserCommand)
+        public async Task<IActionResult> Register([FromBody] UserForRegisterDto userForRegisterDto)
         {
-           
-            TokenDto result = await Mediator.Send(registerUserCommand);
+            RegisterUserCommand userCommand = new RegisterUserCommand()
+            {
+                User = userForRegisterDto,
+                IpAddress = GetIpAddress()
+            };
+
+            RegisteredDto result = await Mediator.Send(userCommand);
+            SetRefreshTokenToCookie(result.RefreshToken);
             return Created("", result.AccessToken);
         }
+
+        private void SetRefreshTokenToCookie(RefreshToken refreshToken)
+        {
+            CookieOptions cookieOptions = new() { HttpOnly = true, Expires = DateTime.Now.AddDays(7) };
+            Response.Cookies.Append("refreshToken", refreshToken.Token, cookieOptions);
+        }
+
 
         [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody] LoginUserCommand loginUserCommand)
